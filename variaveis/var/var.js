00fabd349df0d56da7ad8c9030c78e7bc7019b60
André Luiz <andre.doctors@gmail.com>
2022-04-02 18:31:32 -0300

    termino do estudo basico do var

--- variaveis/var/var.js
+++ variaveis/var/var.js
@@ -15,3 +15,12 @@ var nome; // exemplo de variavel var não declarada que gera o resultado (undefi
 // para ver o reultado dessa variavel usamos o comando a baixo
 console.log(typeof nome);
 // para ver o resultado desse comando abra o arquivo var.html e siga os passos que ele pede
+
+//Você pode redeclarar a variável várias vezes, substituindo-a:
+var variavel1 = 13; // aqui foi declarado a varivel
+var variavel1 = 15; // aqui foi declarada a mesma varivel mas com valor diferente, assim alterando o valor da variavel
+console.log(variavel1);
+
+// Podemos declarar várias variáveis ​​de uma só vez na mesma instrução:
+var nome = 'Andre', idade = 32, altura = 1.80; 
+// Desta forma, mas a maioria das consulta que faço, os prefessores nao indicam.
