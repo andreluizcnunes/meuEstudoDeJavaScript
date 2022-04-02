e6aa4326942546429aae506358650e4d302b9c32
André Luiz <andre.doctors@gmail.com>
2022-04-02 17:58:33 -0300

    alteração de pastas arquivos para melhoor organização do estudo

---
+++ variaveis/var/var.js
@@ -0,0 +1,17 @@
+// Variaveis
+
+//Regras para variaveis
+
+/* 
+(var) é a versão mais antiga de variavel, ela era unica até ES2015 (ES é abreviação de ecmascript - Assim,
+   em poucas palavras, podemos dizer que ECMAScript é uma especificação de linguagem, ou seja, ela define 
+   os padrões para uma linguagem de programação, e o JavaScript é a implementação desses padrões.),
+   bom voltando a variavel (var) ela tem algumas regras:
+
+   1- Se você esquecer de adicionar (var), estará atribuindo um valor a uma variável não declarada e os resultados é (undefined).
+*/
+
+var nome; // exemplo de variavel var não declarada que gera o resultado (undefined).
+// para ver o reultado dessa variavel usamos o comando a baixo
+console.log(typeof nome);
+// para ver o resultado desse comando abra o arquivo var.html e siga os passos que ele pede
