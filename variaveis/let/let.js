fd8e3e928eb2e84e1b2d284e8789f1432017f72a
André Luiz <andre.doctors@gmail.com>
2022-04-02 18:02:15 -0300

    alteração no js do let do const

--- variaveis/let/let.js
+++ variaveis/let/let.js
@@ -2,24 +2,5 @@
 
 //Regras para variaveis
 
-/* 
-(var) é a versão mais antiga de variavel, ela era unica até ES2015 (ES é abreviação de ecmascript - Assim,
-   em poucas palavras, podemos dizer que ECMAScript é uma especificação de linguagem, ou seja, ela define 
-   os padrões para uma linguagem de programação, e o JavaScript é a implementação desses padrões.),
-   bom voltando a variavel (var) ela tem algumas regras:
-
-   1- Se você esquecer de adicionar (var), estará atribuindo um valor a uma variável não declarada e os resultados é (undefined).
-*/
-
-// exemplo de variavel var não declarada.
-
-var nome;
-
-var nomeVar = "André";
-  console.log("Usando o var: ",  nomeVar);
-
 let nomeLet = "André";
   console.log("Usando o let: ", nomeLet);
-
-const nomeConst = "André";
-  console.log("Usando o const: ", nomeConst);
